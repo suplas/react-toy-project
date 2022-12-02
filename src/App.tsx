4355774c35a1dee16603f246dc907d8f981e0719
suplas <suplas@naver.com>
2022-12-03 01:01:13 +0900

    eslint delete and setting update
    eslint delete and setting update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import Button from '@mui/material/Button'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <p>12321321</p>
       </header>
     </div>
   )
